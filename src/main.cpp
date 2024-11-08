738ce33cba7e5b94d931b632beef0ebaf38564fc
VIAUD Nathan <nathan.viaud@inria.fr>
2024-11-08 11:09:10 +0100

    improvement: webserver

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,9 @@ unsigned long lastSensorRead = 0;
 unsigned long lastBlink = 0;
 uint16_t currentBlinkInterval = 0;
 bool ledState = false;
-int lastDistance = 0;
+int lastDistance = 0; 
+unsigned int collisions = 0;
+bool collided = false;
 
 void setup() {
     pinMode(PIN_LED, OUTPUT);
@@ -108,6 +110,10 @@ void loop() {
     if (currentMillis - lastSensorRead >= SENSOR_READ_INTERVAL) {
         lastDistance = distanceSensor.measureDistanceCm();
         currentBlinkInterval = getBlinkFrequency(lastDistance);
+        if (lastDistance <= 1 && lastDistance > 0 && !collided) {
+            collisions ++;
+            collided = true; 
+        } else if (collided && lastDistance > 0) { collided = false; }
 
         lastSensorRead = currentMillis;
     }
@@ -119,42 +125,63 @@ void handleRoot() {
     String html = R"(
     <!DOCTYPE html>
     <html>
-    <head>
-        <title>Distance Sensor Monitor</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-            body { font-family: Arial; text-align: center; margin: 20px; }
-            #distance { font-size: 24px; margin: 20px 0; }
-            #blinkFreq { font-size: 18px; }
-        </style>
-        <script>
-            function updateData() {
-                fetch('/data')
-                    .then(response => response.json())
-                    .then(data => {
-                        document.getElementById('distance').innerHTML = 
-                            'Distance: ' + data.distance + ' cm';
-                        document.getElementById('blinkFreq').innerHTML = 
-                            'Blink Frequency: ' + data.blinkInterval + ' ms';
-                    });
-            }
-            setInterval(updateData, 500);
-        </script>
-    </head>
-    <body>
-        <h1>Distance Sensor Monitor</h1>
-        <div id="distance">Loading...</div>
-        <div id="blinkFreq"></div>
-    </body>
+        <head>
+            <title>Bat radar</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" />
+            <script>
+                function updateData() {
+                    fetch('/data')
+                        .then(response => response.json())
+                        .then(data => {
+                            document.getElementById('distance').innerHTML = 
+                                'Distance: ' + data.distance + ' cm';
+                            document.getElementById('collisions').innerHTML = data.collisions;
+
+                            let color = "#14b8a6"
+                            if (data.distance < 25) {
+                                color = "#ef4444";
+                            } else if (data.distance < 50) {
+                                color = "#f59e0b";
+                            }
+
+                            const distanceBar = document.getElementById('distance-bar');
+                            distanceBar.style = 'width: ' + data['distance-bar'] + '%';
+                            distanceBar.style.backgroundColor = color;
+                        });
+                }
+                setInterval(updateData, 500);
+            </script>
+        </head>
+        <body class="text-center p-4 flex flex-col gap-5">
+            <h1 class="text-4xl font-bold">Bat Radar</h1>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                <div class="shadow p-4 rounded-lg col-span-1 sm:col-span-2 border border-slate-200">
+                    <h2 id="distance" class="text-2xl mb-3">Distance: <span id="distance">0 cm</span></h2>
+                    <div class='w-full bg-gray-100 rounded-3xl h-2.5'>
+                        <div id="distance-bar" class='h-2.5 rounded-3xl transition-all' style='width: 0%'></div>
+                    </div>
+                </div> 
+                <div class="shadow p-4 rounded-lg border border-slate-200">
+                   <h2 class="text-2xl"><span id="collisions">0</span> Collisions</h2> 
+                </div>
+            </div>
+        </body>
     </html>
     )";
     server.send(200, "text/html", html);
 }
 
 void handleData() {
-    StaticJsonDocument<200> doc;    
+    JsonDocument doc;    
     doc["distance"] = lastDistance;
-    doc["blinkInterval"] = currentBlinkInterval;
+    doc["collisions"] = collisions;
+    // the closer the distance is to 0 the higher the progress bar is
+    if (lastDistance > 150) {
+        doc["distance-bar"] = 0;
+    } else {
+        doc["distance-bar"] = map(lastDistance, 0, 150, 100, 0);
+    }
     
     String response;
     serializeJson(doc, response);
